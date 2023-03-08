a8af4275428e964588abae38ae9d5d269579cfba
Nakaroto <vnndong123@gmail.com>
2023-03-09 00:39:26 +0700

    fix add account

--- config/config.php
+++ config/config.php
@@ -56,11 +56,10 @@ class CMSNT
         $value_list = '';
         foreach ($data as $key => $value)
         {
-            $field_list .= ",$key";
+            $field_list .= ",`$key`";
             $value_list .= ",'".mysqli_real_escape_string($this->ketnoi, $value)."'";
         }
         $sql = 'INSERT INTO `'.$table. '`('.trim($field_list, ',').') VALUES ('.trim($value_list, ',').')';
- 
         return mysqli_query($this->ketnoi, $sql);
     }
     function update($table, $data, $where)
